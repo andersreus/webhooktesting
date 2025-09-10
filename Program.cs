a9ed9f7870d53a3da66b26b46b6d7f4807ab5e51
andersreus <arh@umbraco.dk>
2025-09-10 16:40:26 +0200

    Added check for already imported tvshows and other.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using Umbraco.Headless.Client.Net.Delivery;
 using Umbraco.Headless.Client.Net.Management;
-using Umbraco.Headless.Client.Net.Management.Models;
+using Content = Umbraco.Headless.Client.Net.Management.Models.Content;
 
 const string ProjectAlias = "webhookmaze";
 const string ApiKey = "zcJBg3NZmcWeR8Dm4jD5";
@@ -11,11 +12,12 @@ const string SummaryPropertyAlias = "summary";
 const string AllTvShowsContentNodeKey = "3c0a7d4d-4fc9-4b38-9b24-cea3a487c00d";
 
 ContentManagementService contentManagementService = new(ProjectAlias, ApiKey);
+ContentDeliveryService contentDeliveryService = new(ProjectAlias, ApiKey);
 
-await GetALlTvShowsFromTvMazeAsync();
+var existingIds = await GetExistingTvShowsFromHeartcoreAsync();
+await GetALlTvShowsFromTvMazeAsync(existingIds);
 
-// Need to pass in ID so it can continue where it left off.
-async Task GetALlTvShowsFromTvMazeAsync()
+async Task GetALlTvShowsFromTvMazeAsync(HashSet<int> existingIds)
 {
     int page = 0;
     HttpClient client = new();
@@ -27,25 +29,28 @@ async Task GetALlTvShowsFromTvMazeAsync()
         if (!response.IsSuccessStatusCode)
             break;
         var jsonStream = await response.Content.ReadAsStreamAsync();
-        TurnTvShowJsonIntoCsharpModels(jsonStream);
+        await TurnTvShowJsonIntoCsharpModels(jsonStream, existingIds);
         page++;
         // await Task.Delay(100);
     }
 }
 
-async Task TurnTvShowJsonIntoCsharpModels(Stream jsonStream)
+async Task TurnTvShowJsonIntoCsharpModels(Stream jsonStream, HashSet<int> existingIds)
 {
     List<TvModel> tvshows = await JsonSerializer.DeserializeAsync<List<TvModel>>(jsonStream);
-    CreateTvShowContentAsync(tvshows).GetAwaiter().GetResult();
+    var newTvShows = tvshows?.Where(show => !existingIds.Contains(show.Id)).ToList() ?? new List<TvModel>();
+    if (newTvShows.Any())
+    {
+        await CreateTvShowContentAsync(newTvShows);
+    }
 }
 
 async Task CreateTvShowContentAsync(List<TvModel> tvshows)
 {
-    // Go to backoffice and add all languages to the string array
-    string[] cultures = new[] { "en-US", "da-DK" };
+    string[] cultures = new[] { "en-US", "da-DK", "nl-NL", "fr-FR", "de-DE", "it-IT", "el-GR", "hi-IN", "ja-JP", "vi-VN" };
     foreach (var tvshow in tvshows)
     {
-        var content = new Content
+        var content = new Content()
         {
             ContentTypeAlias = ContentTypeAlias,
             ParentId = new Guid(AllTvShowsContentNodeKey)
@@ -57,21 +62,38 @@ async Task CreateTvShowContentAsync(List<TvModel> tvshows)
                 "$invariant", tvshow.Id
             }
         });
+
+        var translatedSummary = new Dictionary<string, object>();
         foreach (var culture in cultures)
         {
-            content.Properties.Add(SummaryPropertyAlias, new Dictionary<string, object>
-            {
-                {
-                    culture, tvshow.Summary ?? "No summary for this tvshow"
-                }
-            });
+            translatedSummary[culture] = tvshow.Summary ?? "No summary for this tvshow";
         }
+        content.Properties.Add(SummaryPropertyAlias, translatedSummary);
         
         var createdContent = await contentManagementService.Content.Create(content);
         await contentManagementService.Content.Publish(createdContent.Id);
         
         Console.WriteLine($"Created and published content: {createdContent.Name.First().Value}");
     }
+}
+
+async Task<HashSet<int>> GetExistingTvShowsFromHeartcoreAsync()
+{
+    var existingIds = new HashSet<int>();
+    var existingChilds = await contentDeliveryService.Content.GetChildren(Guid.Parse(AllTvShowsContentNodeKey));
+
+    foreach (var tvshow in existingChilds.Content.Items)
+    {
+        // no fucking clue if this works
+        if (tvshow?.Properties != null && tvshow.Properties.TryGetValue(IdPropertyAlias, out var idObj)
+            && idObj is Dictionary<string, object> dict
+            && dict.TryGetValue("$invariant", out var idVal)
+            && int.TryParse(idVal.ToString(), out var id))
+        {
+            existingIds.Add(id);
+        }
+    }
+    return existingIds;
 }
 
 public class TvModel
